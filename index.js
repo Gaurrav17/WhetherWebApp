87022712067297ccdb4ddfb665414a4d2f7305b9
Gaurrav17 <gauravsrivastava17cu@gmail.com>
2024-04-02 01:14:03 +0530

    Improved Little CSS

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const API_ID = "82fb8cae0edea5f3833e9857a3d6f172";
 
-
-/*app.set('views', './views');
-app.set('view engine', 'ejs');*/
 
 app.get("/", (req, res) => {
     res.render("indexe.ejs");
@@ -25,25 +21,12 @@ app.post("/submit", async (req, res) => {
     const cityname = req.body.city;
     try {
       
-      console.log(cityname);
       const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=82fb8cae0edea5f3833e9857a3d6f172&units=metric`); 
-           //const data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=82fb8cae0edea5f3833e9857a3d6f172&units=metric1`)   
-          //const weatherData = response.data;
-         //const temp  = weatherData.main[0].temp;
-        //console.log(temp);
-       //   console.log(response.data)
-      //   const wt = JSON.parse(response);
-     //   console.log(wt.weather.main.temp);
-     /*const weatherData = */
-     //console.log(response.body);
-       // const wt =  weatherData.main.temp
-       console.log(response.data.list[0].main.temp);
-       //const weatherData = response.data.list[0].main.temp;
-
-     //console.log(weatherData);
-      res.render("indexe.ejs", {
-        /*tep: "response",*/
+   
+      res.render("indexe.ejs", { 
         weatherData: response.data.list[0].main.temp,
+        Humid: response.data.list[0].main.humidity,
+        wnd: response.data.list[0].wind.speed, 
         cty: cityname,
       });
     } catch (error) {
